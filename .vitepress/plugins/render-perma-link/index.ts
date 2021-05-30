93a105029127073052efc440fd29aba0f8411b2b
QC-L <github@liqichang.com>
2021-05-31 00:46:20 +0800

    lint: fix code

--- .vitepress/plugins/render-perma-link/index.ts
+++ .vitepress/plugins/render-perma-link/index.ts
@@ -6,8 +6,8 @@ const renderPermalink = (slug: any, opts: any, state: any, permalink: any) => {
       .children
       .filter((token: any) => token.type === 'text' || token.type === 'code_inline')
       .reduce((acc: any, t: any) => acc + t.content, '')
-    const match = /^.+(\s*\{#([a-z0-9\-_]+?)\}\s*)$/.exec(title);
-    slug = match ? match[2] : slug;
+    const match = /^.+(\s*\{#([a-z0-9\-_]+?)\}\s*)$/.exec(title)
+    slug = match ? match[2] : slug
     token.attrSet('id', slug)
     const space = () => Object.assign(new state.Token('text', '', 0), { content: ' ' })
 
@@ -22,11 +22,11 @@ const renderPermalink = (slug: any, opts: any, state: any, permalink: any) => {
       Object.assign(new state.Token('html_block', '', 0), { content: opts.permalinkSymbol }),
       new state.Token('link_close', 'a', -1),
     ]
-    if (opts.permalinkSpace) {
+    if (opts.permalinkSpace)
       linkTokens[!opts.permalinkBefore ? 'push' : 'unshift'](space())
-    }
     state.tokens[permalink + 1].children[opts.permalinkBefore ? 'push' : 'unshift'](...linkTokens)
-  } catch (e) {}
+  }
+  catch (e) {}
 }
 
 export default renderPermalink
